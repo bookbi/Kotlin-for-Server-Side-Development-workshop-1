5e5cfc28909b0660a8dbbd6514c5293b51a07594
BOOKBIG <yanapatinkham@gmail.com>
2025-06-29 13:58:14 +0700

    Workshop 3 version 100%

--- src/test/kotlin/WorkshopTest.kt
+++ src/test/kotlin/WorkshopTest.kt
@@ -1,8 +1,23 @@
+import org.example.Product
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import org.example.celsiusToFahrenheit
+import org.example.kilometersToMiles
+import org.example.calculateTotalElectronicsPriceOver500
+import org.example.countElectronicsOver500
+
 
 class WorkshopTest {
 
+    val products = listOf(
+        Product("Laptop", 35000.0, "Electronics"),
+        Product("Smartphone", 25000.0, "Electronics"),
+        Product("T-shirt", 450.0, "Apparel"),
+        Product("Monitor", 7500.0, "Electronics"),
+        Product("Keyboard", 499.0, "Electronics"), // ราคาไม่เกิน 500
+        Product("Jeans", 1200.0, "Apparel"),
+        Product("Headphones", 1800.0, "Electronics")  // ตรงตามเงื่อนไข
+    )
 
     // --- Tests for Workshop #1: Unit Converter ---
 
@@ -79,21 +94,21 @@ class WorkshopTest {
         val count = countElectronicsOver500(products)
         assertEquals(3, count, "There should be 3 Electronics items over 500")
     }
-
-
-
-    // --- Tests for Workshop #1: Unit Converter End ---
-
-    // --- Tests for Workshop #2: Data Analysis Pipeline ---
-    // ทำการแก้ไขไฟล์ Workshop2.kt ให้มีฟังก์ชันที่ต้องการทดสอบ
-    // เช่น ฟังก์ชันที่คำนวณผลรวมราคาสินค้า Electronics ที่ราคา > 500 บาท
-    // ในที่นี้จะสมมุติว่ามีฟังก์ชันชื่อ calculateTotalElectronicsPriceOver500 ที่รับ List<Product> และคืนค่า Double
-    // จงเขียน test cases สำหรับฟังก์ชันนี้ โดยตรวจสอบผลรวมราคาสินค้า Electronics ที่ราคา > 500 บาท
-    // 🚨
-
-    // จงเขียน test cases เช็คจำนวนสินค้าที่อยู่ในหมวด 'Electronics' และมีราคามากกว่า 500 บาท
-    // 🚨
-
-
-    // --- Tests for Workshop #2: Data Analysis Pipeline End ---
 }
+
+
+
+// --- Tests for Workshop #1: Unit Converter End ---
+
+// --- Tests for Workshop #2: Data Analysis Pipeline ---
+// ทำการแก้ไขไฟล์ Workshop2.kt ให้มีฟังก์ชันที่ต้องการทดสอบ
+// เช่น ฟังก์ชันที่คำนวณผลรวมราคาสินค้า Electronics ที่ราคา > 500 บาท
+// ในที่นี้จะสมมุติว่ามีฟังก์ชันชื่อ calculateTotalElectronicsPriceOver500 ที่รับ List<Product> และคืนค่า Double
+// จงเขียน test cases สำหรับฟังก์ชันนี้ โดยตรวจสอบผลรวมราคาสินค้า Electronics ที่ราคา > 500 บาท
+// 🚨
+
+// จงเขียน test cases เช็คจำนวนสินค้าที่อยู่ในหมวด 'Electronics' และมีราคามากกว่า 500 บาท
+// 🚨
+
+
+//
